2e74e3ca710d11c70a728e34d8781a936559c368
Paul Beckwith <303dog@gmail.com>
2020-05-15 20:24:50 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,7 +3,7 @@ class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
   
-  def initialize(name)
+  def initialize(account_name)
   @name = account_name
   @balance = 1000
   @status = "open"
