75f42da8542e508841ba0a59e409fdc2b5024658
Paul Beckwith <303dog@gmail.com>
2020-05-15 21:39:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,15 +13,15 @@ class Transfer
     @sender.valid? && @receiver.valid? ? true : false
   end 
   
-  def execute_transaction
+   def execute_transaction
     if @sender.balance > @amount && @status == "pending"
-       @sender.balance -= @amount
-       @receiver.balance += @amount
-       @status = "complete"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
     else
-       @status = "rejected"
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
-    end 
+    end
   end 
   
   def reverse_transfer
